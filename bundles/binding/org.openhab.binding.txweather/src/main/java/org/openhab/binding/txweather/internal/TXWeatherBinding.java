988fbd03202584e6a3eb5c7718e6a6ff8f506665
Joachim Wilke <git@joachim-wilke.de>
2014-01-19 12:06:00 +0100

    Initial commit of txweather binding

---
+++ bundles/binding/org.openhab.binding.txweather/src/main/java/org/openhab/binding/txweather/internal/TXWeatherBinding.java
@@ -0,0 +1,221 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.txweather.internal;
+
+import java.util.Dictionary;
+
+import org.apache.commons.lang.StringUtils;
+import org.openhab.binding.txweather.TXWeatherBindingConfig;
+import org.openhab.binding.txweather.TXWeatherBindingProvider;
+import org.openhab.core.binding.AbstractActiveBinding;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.openhab.io.transport.cul.CULDeviceException;
+import org.openhab.io.transport.cul.CULHandler;
+import org.openhab.io.transport.cul.CULListener;
+import org.openhab.io.transport.cul.CULManager;
+import org.openhab.io.transport.cul.CULMode;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+	
+
+/**
+ * Implement this class if you are going create an actively polling service
+ * like querying a Website/Device.
+ * 
+ * @author MaJo
+ * @since 1.4.0
+ */
+public class TXWeatherBinding extends AbstractActiveBinding<TXWeatherBindingProvider> implements ManagedService, CULListener {
+
+	private static final Logger logger = 
+		LoggerFactory.getLogger(TXWeatherBinding.class);
+	
+	private CULHandler cul;
+	private final static String KEY_DEVICE_NAME = "device";
+	private String deviceName;
+	
+	/** 
+	 * the refresh interval which is used to poll values from the TXWeather
+	 * server (optional, defaults to 60000ms)
+	 */
+	private long refreshInterval = 60000;
+	
+	
+	public TXWeatherBinding() {
+	}
+		
+	
+	public void activate() {
+		logger.debug("Activating TXWeather binding");
+	}
+
+	private void getCULHandler() {
+		try {
+			logger.debug("Opening CUL device on " + deviceName);
+			cul = CULManager.getOpenCULHandler(deviceName, CULMode.SLOW_RF);
+			cul.registerListener(this);
+		} catch (CULDeviceException e) {
+			logger.error("Can't open cul device", e);
+			cul = null;
+		}
+	}
+
+	
+	public void deactivate() {
+		// deallocate resources here that are no longer needed and 
+		// should be reset when activating this binding again
+		logger.debug("Deactivating FS20 binding");
+		cul.unregisterListener(this);
+		CULManager.close(cul);
+}
+
+	
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	protected long getRefreshInterval() {
+		return refreshInterval;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	protected String getName() {
+		return "TXWeather Refresh Service";
+	}
+	
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	protected void execute() {
+		// the frequently executed code (polling) goes here ...
+//		logger.debug("execute() method is called!");
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	protected void internalReceiveCommand(String itemName, Command command) {
+		// the code being executed when a command was sent on the openHAB
+		// event bus goes here. This method is only called if one of the 
+		// BindingProviders provide a binding for the given 'itemName'.
+		logger.debug("internalReceiveCommand() is called!");
+	}
+	
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	protected void internalReceiveUpdate(String itemName, State newState) {
+		// the code being executed when a state was sent on the openHAB
+		// event bus goes here. This method is only called if one of the 
+		// BindingProviders provide a binding for the given 'itemName'.
+		logger.debug("internalReceiveCommand() is called!");
+	}
+		
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void updated(Dictionary<String, ?> config) throws ConfigurationException {
+		logger.debug("Received new config");
+		if (config != null) {
+
+			// to override the default refresh interval one has to add a
+			// parameter to openhab.cfg like
+			// <bindingName>:refresh=<intervalInMs>
+			String refreshIntervalString = (String) config.get("refresh");
+			if (StringUtils.isNotBlank(refreshIntervalString)) {
+				refreshInterval = Long.parseLong(refreshIntervalString);
+			}
+			String deviceName = (String) config.get(KEY_DEVICE_NAME);
+			if (StringUtils.isEmpty(deviceName)) {
+				logger.error("No device name configured");
+				setProperlyConfigured(false);
+				throw new ConfigurationException(KEY_DEVICE_NAME,
+						"The device name can't be empty");
+			} else {
+				setNewDeviceName(deviceName);
+			}
+
+			setProperlyConfigured(true);
+			// read further config parameters here ...
+
+		}
+	}
+
+	private void setNewDeviceName(String deviceName) {
+		if (cul != null) {
+			CULManager.close(cul);
+		}
+		this.deviceName = deviceName;
+		getCULHandler();
+	}
+
+
+	@Override
+	public void dataReceived(String data) {
+		// It is possible that we see here messages of other protocols
+		if (data.startsWith("tA")) {
+			logger.debug("Received TXWeather message: " + data);
+			handleReceivedMessage(data);
+		}
+	}
+
+	private void handleReceivedMessage(String message) {
+		// TX3 temperature sensor, example tA0396316381A
+		String bindingConfig = message.substring(2, 5);
+		int type  = Integer.parseInt(message.substring(2, 3),16);
+		int address = Integer.parseInt(message.substring(3, 5),16) & 0xFE;
+		double value = 0.0;
+		switch (type){
+			case 0x00:{ //temperature
+		 		value = (Integer.parseInt(message.substring(5, 8)) - 500) / 10.0;	
+		 		break;
+			}
+			case 0x01:{ // pressure
+				value = (Integer.parseInt(message.substring(5, 10)) + 50000) / 100.0;
+				break;
+			}
+			case 0x0E:{ //humidity
+				value = (Integer.parseInt(message.substring(5, 8))) / 10.0;
+				break;
+			}
+		}
+
+		TXWeatherBindingConfig config = null;
+		for (TXWeatherBindingProvider provider : providers) {
+			config = provider.getConfigForSensor(type, address);
+			if (config != null) {
+				break;
+			}
+		}
+		if (config != null) {
+			eventPublisher.postUpdate(config.getItem().getName(),
+					new DecimalType(value));
+		} else {
+			logger.debug("Received message for unknown binding config " + bindingConfig);
+		}
+	}
+
+	@Override
+	public void error(Exception e) {
+		logger.error("Error while communicating with CUL", e);
+	}
+	
+
+}
